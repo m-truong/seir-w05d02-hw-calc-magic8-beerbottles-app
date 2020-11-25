b952c45de347ed98f1c9ff8bf7ab631469987dcc
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-11-24 21:01:55 -0500

    Magic 8 Ball express application created

--- homework/server.js
+++ homework/server.js
@@ -38,7 +38,19 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 })
 
 // ===============
-// 
+// Magic 8 Ball //
+// ===============
+
+// should expect a String 
+    // can URL param be any parameter? 
+app.get('/magic/:question', (req, res) => {
+    // store question param as const ques
+    const ques = req.params.question;
+    const magicArr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    const answer = `${ques}, ${magicArr[Math.floor(Math.random()*magicArr.length)]}`
+    res.send(`<h1>${answer}</h1>`)
+})
+
 
 // default port 3000
 app.listen(3000, () => {
